f7aa4e0a4156382fee53d87eb201ca9cc6d296a8
Tamas Papp <tamas.papp@rtfm.co.hu>
2014-09-28 20:41:56 +0200

    second change

--- check-gen.sh
+++ check-gen.sh
@@ -6,5 +6,5 @@ IFS='
 TLD='(com|net)'
 
 for domain in `cat domains.txt`;do
-	echo "/[@/. ]${domain}([?!#,. ])/"   DISCARD DROP: ${i}|sed "s;TLD;"${TLD};"
+	echo "/${domain}(/, )?/"   DISCARD DROP: ${i}|sed "s;TLD;${TLD};"
 done
