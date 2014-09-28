a82bc0944519bc2b87b24177e3caf8189f7e9b13
Tamas Papp <tamas.papp@rtfm.co.hu>
2014-09-28 21:39:24 +0200

    fix common regexp

--- check-gen.sh
+++ check-gen.sh
@@ -6,5 +6,5 @@ IFS='
 TLD='(com|net)'
 
 for domain in `cat domains.txt`;do
-	echo "/${domain}(/, )?/"   DISCARD DROP: ${domain}|sed "s;TLD;${TLD};g"
+	echo "/${domain}([\/\,\ ])?/"   DISCARD DROP: ${domain}|sed "s;TLD;${TLD};g"
 done
