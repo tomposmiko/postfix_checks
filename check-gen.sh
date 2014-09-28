2191a1d3d9fdc5b84091aeff4f718f871665ceba
Tamas Papp <tamas.papp@rtfm.co.hu>
2014-09-28 21:07:07 +0200

    add README.txt

--- check-gen.sh
+++ check-gen.sh
@@ -6,5 +6,5 @@ IFS='
 TLD='(com|net)'
 
 for domain in `cat domains.txt`;do
-	echo "/${domain}(/, )?/"   DISCARD DROP: ${i}|sed "s;TLD;${TLD};"
+	echo "/${domain}(/, )?/"   DISCARD DROP: ${domain}|sed "s;TLD;${TLD};g"
 done
