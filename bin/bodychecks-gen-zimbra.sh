661ddd926ad8ff29e032cb10c296839062c4fa95
Tamas Papp <tamas.papp@rtfm.co.hu>
2014-09-30 15:41:49 +0200

    update zimbra script

--- bin/bodychecks-gen-zimbra.sh
+++ bin/bodychecks-gen-zimbra.sh
@@ -7,10 +7,22 @@ dir_postfix_checks="/opt/etc/postfix_checks"
 TLD='(com|net)'
 cd $dir_postfix_checks || exit 1
 
-if ! git pull|grep -q 'Already up-to-date';
+if git pull|grep -q 'Already up-to-date';
+	then
+		updated=0
+	else
+		updated=1
+fi
+
+if [ x$1 == x-f ];
+	then
+		updated=1
+fi
+
+if [ $updated -eq 1 ];
 	then
 		for domain in `grep -v ^\# domains.txt`;do
-			echo "/${domain}([\/\,\ ])?/	WARN 'SPAM: ${domain}'"|sed "s;TLD;${TLD};g"
+			echo "/${domain}([\/\,\ ])?/   WARN 'SPAM: `echo ${domain}|sed 's;\[\@\\\/\\\.\\\ \];;'`'"|sed "s;TLD;${TLD};g"
 		done > body_checks
 		sudo /opt/zimbra/postfix/sbin/postfix reload
 fi
