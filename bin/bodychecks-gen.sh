1d47db976ab19db2f8529fa805f845bf9e2254e8
Tamas Papp <tamas.papp@rtfm.co.hu>
2014-09-30 15:27:56 +0200

    add switch -f -> force

--- bin/bodychecks-gen.sh
+++ bin/bodychecks-gen.sh
@@ -7,7 +7,19 @@ dir_postfix_checks="/etc/postfix/postfix_checks"
 TLD='(com|net)'
 cd $dir_postfix_checks || exit 1
 
-if ! git pull|grep -q 'Already up-to-date';
+if git pull|grep -q 'Already up-to-date';
+	then
+		updated=0
+	else
+		updated=1
+fi
+
+if [ $1 == -f ];
+	then
+		updated=1
+fi
+
+if [ $updated -eq 1 ];
 	then
 		for domain in `grep -v ^\# domains.txt`;do
 			echo "/${domain}([\/\,\ ])?/   REJECT 'SPAM: `echo ${domain}|sed 's;\[\@\\\/\\\.\\\ \];;'`'"|sed "s;TLD;${TLD};g"
