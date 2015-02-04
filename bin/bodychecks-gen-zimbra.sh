33b5dd0bf84819beadbbdacce15c5fd361e74ce3
Tamas Papp <tamas.papp@rtfm.co.hu>
2015-02-04 10:51:54 +0100

    zimbrazie it

--- bin/bodychecks-gen-zimbra.sh
+++ bin/bodychecks-gen-zimbra.sh
@@ -19,10 +19,20 @@ if [ x$1 == x-f ];
 		updated=1
 fi
 
+
 if [ $updated -eq 1 ];
 	then
-		for domain in `grep -v ^\# domains.txt`;do
-			echo "/${domain}([,>\*\/\ ]|$)/   REJECT 'SPAM: `echo ${domain}|sed 's;\[\@\\\/\\\.\\\ \];;'`'"|sed "s;TLD;${TLD};g"
-		done > body_checks
+		for url in `grep -v ^\# urls.txt`;do
+			echo "/${domain}([,>\*\/\ ]|$)/   REJECT 'SPAM: `echo ${domain}|sed 's,\[\@\\\/\\\.\\\ \],,'`'"|sed "s,TLD,${TLD},g"
+		done > body_checks_urls
 		sudo /opt/zimbra/postfix/sbin/postfix reload
 fi
+
+if [ $updated -eq 1 ];
+    then
+        for entry in `grep -v ^\# misc.txt`;do
+            echo "/${entry}/   REJECT 'SPAM: ${entry}'"|sed "s;TLD;${TLD};g"
+        done > body_checks_misc
+        sudo /opt/zimbra/postfix/sbin/postfix reload
+fi
+
