11e3500e41f273356c7d70885d3e1c7a887c8761
Tamas Papp <tamas.papp@rtfm.co.hu>
2020-04-18 22:02:25 +0200

    align with shellchecker

--- bin/bodychecks-gen.sh
+++ bin/bodychecks-gen.sh
@@ -8,22 +8,22 @@ dir_postfix_checks="/etc/postfix/postfix_checks"
 TLD='(com|eu|net)'
 cd $dir_postfix_checks || exit 1
 
-if git pull | egrep -q '(Already up to date|Already up-to-date)';
+if git pull | grep -E -q '(Already up to date|Already up-to-date)';
 	then
 		updated=0
 	else
 		updated=1
 fi
 
-if [ x$1 == x-f ];
+if [ "x$1" == x-f ];
 	then
 		updated=1
 fi
 
-if [ $updated -eq 1 ];
+if [ "$updated" -eq 1 ];
 	then
-		for url in `grep -v ^\# urls.txt`;do
-			echo "/${url}([,>\*\/\ ]|$)/   REJECT 'SPAM: `echo ${url}|sed 's,\[\@\\\/\\\.\\\ \],,' | tr -d \$`'"|sed "s,TLD,${TLD},g"
+            for url in $(grep -v ^\# urls.txt);do
+                    echo "/${url}([,>\*\/\ ]|$)/   REJECT 'SPAM: $(echo "${url}"|sed 's,\[\@\\\/\\\.\\\ \],,' | tr -d \$)'"|sed "s,TLD,${TLD},g"
 		done > body_checks_urls
 		#echo > body_checks_urls
 		sudo /usr/sbin/postfix reload
@@ -31,8 +31,8 @@ fi
 
 if [ $updated -eq 1 ];
     then
-        for misc in `grep -v ^\# misc.txt`;do
-            echo "/${misc}/   REJECT 'SPAM: `echo ${misc} | tr -d \$`'"|sed "s;TLD;${TLD};g"
+            for misc in $(grep -v ^\# misc.txt);do
+                    echo "/${misc}/   REJECT 'SPAM: $(echo "${misc}" | tr -d \$)'"|sed "s;TLD;${TLD};g"
         done > body_checks_misc
 
 		#echo > body_checks_misc
